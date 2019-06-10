ca01f0e079ef07c721c803355ddbafdc94e2a2cd
anmesamo90 <50255268+anmesamo90@users.noreply.github.com>
2019-06-10 18:54:51 -0500

    arreglos

--- q09.py
+++ q09.py
@@ -5,9 +5,20 @@
 ## 
 
 
+##import pandas as pd
+##import numpy as np
+##import matplotlib.pyplot as plt
+##%matplotlib inline
+##df = pd.read_csv("tbl0.tsv", sep="\t")
+##df[['_c1','_c2']].sort_values('_c2',ascending = True).groupby('_c1')[('_c2')].apply(list)
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 %matplotlib inline
 df = pd.read_csv("tbl0.tsv", sep="\t")
-df[['_c1','_c2']].sort_values('_c2',ascending = True).groupby('_c1')[('_c2')].apply(list)
+df['_c2']=df['_c2'].apply(str)
+df2=df.sort_values('_c2',ascending = True).groupby('_c1')['_c2'].apply(lambda x: ':'.join(x))
+
+df3=pd.DataFrame({'_c0':df2.index, 'lista':df2.values})
+print (df3)
--- q10.py
+++ q10.py
@@ -3,9 +3,20 @@
 ## separada por ',' de los valores de la columna _c4
 ## de la tabla tbl1.tsv
 ## 
+##import pandas as pd
+##import numpy as np
+##import matplotlib.pyplot as plt
+##%matplotlib inline
+##df = pd.read_csv("tbl1.tsv", sep="\t")
+##df[['_c0','_c4']].sort_values('_c4',ascending = True).groupby('_c0')[('_c4')].apply(list)
+
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 %matplotlib inline
 df = pd.read_csv("tbl1.tsv", sep="\t")
-df[['_c0','_c4']].sort_values('_c4',ascending = True).groupby('_c0')[('_c4')].apply(list)
+df['_c4']=df['_c4'].apply(str)
+df2=df.sort_values('_c4',ascending = True).groupby('_c0')['_c4'].apply(lambda x: ','.join(x))
+df3=pd.DataFrame({'_c0':df2.index, 'lista':df2.values})
+print (df3)
